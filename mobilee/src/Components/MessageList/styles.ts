155672cd0589ec345e3ec85427c2c3dac096d7e3
Ricardo Filho <ricardinhogiasson16@gmail.com>
2022-01-11 15:59:07 -0300

    mobile

---
+++ mobilee/src/Components/MessageList/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import {COLORS, FONTS} from "../../theme";
+
+export const styles = StyleSheet.create({
+  container: {
+   
+  },
+
+  content:{
+    paddingTop:135,
+    paddingBottom:184,
+  },
+
+});
